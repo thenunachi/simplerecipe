5fae64131a6e868a45d1c44a7b81cade9d37ed55
ThenuKalyan <thenammainachiyappan@ThenuKalyans-MacBook-Air.local>
2025-09-29 15:15:25 -0700

    changed api base route to backend

--- reci-app/frontend/src/api.js
+++ reci-app/frontend/src/api.js
@@ -1,6 +1,6 @@
 // src/api.js
 const API_BASE = process.env.REACT_APP_API_URL;
 export async function getThings() {
-  const res = await fetch(`${API_BASE}/things`);
+  const res = await fetch(`${API_BASE}/recipes`);
   return res.json();
 }
